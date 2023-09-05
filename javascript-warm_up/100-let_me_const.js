4c9802a23b1c59cc0b10b54c1bee34fedf1baaee
mmothiba4 <mmothiba4@gmail.com>
2023-09-05 14:21:22 +0200

    javascript

--- javascript-warm_up/100-let_me_const.js
+++ javascript-warm_up/100-let_me_const.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
 //A fike that modifies the value of myVar to 333.
 
-const myVar = 89
-require('./100_let_me_const')
-console.log(myVar);
-module.exports = function(){
-    require('./100_main')
-    myVar = 333;
-}
+myVar = 333;

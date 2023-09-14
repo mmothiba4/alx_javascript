10b3b03e07865014bacd5229994e597028b3435b
mmothiba4 <mmothiba4@gmail.com>
2023-09-14 08:54:05 +0200

    js network

---
+++ javascript-network/4-completed_tasks.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+request(process.argv[2], function(err, response, body) {
+    if (!err) {
+        const data = JSON.parse(body);
+        const completedUsers = {};
+        data.forEach((element) => {
+            if (element.completed && completedUsers[element.userId] === undefined) {
+                completedUsers[element.userId] = 1;
+            } else if (element.completed) {
+                completedUsers[element.userId] += 1;
+            }
+        });
+            console.log(completedUsers);
+    }
+});

243f2d1d27b5833e96aaacae07938a593722f3ea
mmothiba4 <mmothiba4@gmail.com>
2023-09-21 15:26:43 +0200

    js network

--- javascript_advanced/3-classrooms.js
+++ javascript_advanced/3-classrooms.js
@@ -1,13 +1,17 @@
 #!/usr/bin/node
-function createClassRoom(NumbersOfStudents) {
+function createClassRoom(numbersOfStudents) {
     function studentSeat(seat) {
-        return seat;
+        return function(){
+        return seat};
     }
- const students = []
- let studentSeat = 0
- for (let i = 0; i < numberOfStudents; i++) {
-    return 
- }
- const classRoom = createClassRoom(10)
- console.log(classRoom);
+    let students = [];
+    let numberOfStudents = 0
+    for (let i = 0; i < numberOfStudents; i++) {
+        students.push(studentSeat(i+1)); 
+    }
+    return students;
 }
+let classRoom = createClassRoom(10);
+console.log(classRoom[0]);
+console.log(classRoom[3]);
+console.log(classRoom[9]);

b3421c0bfe51f3c3dded8b2c1000d071b87cb50f
mmothiba4 <mmothiba4@gmail.com>
2023-09-22 14:17:50 +0200

    js advanced

---
+++ javascript_advanced/6-hogwarts.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+
+const studentHogwards = (function () {
+    let privateScore = 0;
+    let name = null;
+
+    function changeScoreBy(points) {
+        privateScore += points;
+    }
+
+    return {
+        setName: function (newName) {
+            name = newName;
+        },
+        rewardStudent: function () {
+            changeScoreBy(1);
+        },
+        penalizeStudent: function () {
+            changeScoreBy(-1);
+        },
+        getScore: function () {
+            return `${name}: ${privateScore}`;
+        },
+    };
+})();
+
+let harry = object.create(studentHogwards);
+harry.setName("Harry");
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+console.log(harry.getScore());
+
+let draco = object.create(studentHogwards);
+draco.setName("Draco");
+draco.rewardStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+console.log(draco.getScore());

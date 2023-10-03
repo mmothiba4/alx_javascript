714d8d3c987a17dc4e4fb51640b6fe5dcac2eb1a
mmothiba4 <mmothiba4@gmail.com>
2023-10-03 16:10:54 +0200

    js validation

--- html_js_validation/0-password-strength.js
+++ html_js_validation/0-password-strength.js
@@ -6,35 +6,40 @@ document.getElementById("passwordForm").addEventListener("submit", function(even
     if (password.length !== 8) {
         // Password is not 8 characters long, display an error message
         document.getElementById("error").textContent = "Password must be exactly 8 characters long.";
-        event.preventDefault(); // Prevent form submission
+        event.preventDefault(); 
+        // Prevent form submission
         return;
     }
 
     if (!/[A-Z]/.test(password)) {
         // Password does not contain at least one uppercase letter, display an error message
         document.getElementById("error").textContent = "Password must contain at least one uppercase letter.";
-        event.preventDefault(); // Prevent form submission
+        event.preventDefault(); 
+        // Prevent form submission
         return;
     }
 
     if (!/[a-z]/.test(password)) {
         // Password does not contain at least one lowercase letter, display an error message
         document.getElementById("error").textContent = "Password must contain at least one lowercase letter.";
-        event.preventDefault(); // Prevent form submission
+        event.preventDefault(); 
+        // Prevent form submission
         return;
     }
 
     if (!/\d/.test(password)) {
         // Password does not contain at least one numeric digit, display an error message
         document.getElementById("error").textContent = "Password must contain at least one numeric digit.";
-        event.preventDefault(); // Prevent form submission
+        event.preventDefault(); 
+        // Prevent form submission
         return;
     }
 
     if (!/[!@#$%^&*]/.test(password)) {
         // Password does not contain at least one special character, display an error message
         document.getElementById("error").textContent = "Password must contain at least one special character (!@#$%^&*).";
-        event.preventDefault(); // Prevent form submission
+        event.preventDefault(); 
+        // Prevent form submission
         return;
     }
 

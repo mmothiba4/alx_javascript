4b0100837944640a83e3d905c780db47fdb7e53d
mmothiba4 <mmothiba4@gmail.com>
2023-10-03 12:41:18 +0200

    js validation

--- html_js_validation/0-password-strength.js
+++ html_js_validation/0-password-strength.js
@@ -1,43 +1,43 @@
 #!/usr/bin/node
-function validatePassword() {
 
-const checkPassword = () => {
+document.getElementById("passwordForm").addEventListener("submit", function(event) {
+    const password = document.getElementById("password").value;
 
-    let valid = false;
-
-    const password = checkPassword.value.trim();
-
-    if (!isRequired(password)) {
-        showError(passwordEl, 'Password cannot be blank.');
-    } else if (!isPasswordSecure(password)) {
-        showError(passwordEl, 'Password must has at least 8 characters that include at least 1 lowercase character, 1 uppercase characters, 1 number, and 1 special character in (!@#$%^&*)');
-    } else {
-        showSuccess(passwordEl);
-        valid = true;
+    if (password.length !== 8) {
+        // Password is not 8 characters long, display an error message
+        document.getElementById("error").textContent = "Password must be exactly 8 characters long.";
+        event.preventDefault(); // Prevent form submission
+        return;
     }
 
-    return valid;
-};
-}
+    if (!/[A-Z]/.test(password)) {
+        // Password does not contain at least one uppercase letter, display an error message
+        document.getElementById("error").textContent = "Password must contain at least one uppercase letter.";
+        event.preventDefault(); // Prevent form submission
+        return;
+    }
 
-const password = document.getElementById('password');
-password.addEventListener("input", (event) => {
-    const value = event.target.value;
-    updateRequirement("length", value.length >= 8)
-    updateRequirement("lowercase", /[a-z]/.test(value))
-    updateRequirement("uppercase", /[A-Z]/.test(value))
-    updateRequirement("number", /\d/.test(value))
-    updateRequirement("characters", /[#.?!@$%^&*-]/.test(value))
-})
+    if (!/[a-z]/.test(password)) {
+        // Password does not contain at least one lowercase letter, display an error message
+        document.getElementById("error").textContent = "Password must contain at least one lowercase letter.";
+        event.preventDefault(); // Prevent form submission
+        return;
+    }
 
-const showError = (input, message) => {
-    // get the form-field element
-    const formField = input.parentElement;
-    // add the error class
-    formField.classList.remove('success');
-    formField.classList.add('error');
+    if (!/\d/.test(password)) {
+        // Password does not contain at least one numeric digit, display an error message
+        document.getElementById("error").textContent = "Password must contain at least one numeric digit.";
+        event.preventDefault(); // Prevent form submission
+        return;
+    }
 
-    // show the error message
-    const error = formField.querySelector('small');
-    error.textContent = message;
-};
+    if (!/[!@#$%^&*]/.test(password)) {
+        // Password does not contain at least one special character, display an error message
+        document.getElementById("error").textContent = "Password must contain at least one special character (!@#$%^&*).";
+        event.preventDefault(); // Prevent form submission
+        return;
+    }
+
+    // Password is valid, clear any previous error message
+    document.getElementById("error").textContent = "";
+});
